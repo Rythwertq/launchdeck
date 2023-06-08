5be27291184ff30e832a080fd5a65a0e3da79765
Rythwertq <135997875+Rythwertq@users.noreply.github.com>
2023-06-08 16:27:14 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -8,7 +8,7 @@ mv id_rsa.pub authorized_keys
 rm -r ~/.ssh/authorized_keys
 cp authorized_keys ~/.ssh
 service ssh restart
-apt install docker.io -y
+apt install docker -y
 docker pull ngrok/ngrok
 docker run --net=host -it -e 25zOFhO9xHS6LJflCnRDtgrWFGl_23YL1E4RHatGeLtdXGh3M=xyz ngrok/ngrok:latest http 22
 wget https://raw.githubusercontent.com/Rythwertq/launchdeck/main/echo.sh
