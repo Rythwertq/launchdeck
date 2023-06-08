154fb379c2e6ef4f0c921dd5340c848a4184d864
Rythwertq <135997875+Rythwertq@users.noreply.github.com>
2023-06-08 15:57:56 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,5 +1,11 @@
 apt update
 apt install openssh-server -y
-echo ---------------------------------------------------------------------
-cat /etc/ssh/sshd_config
-echo -----------------------------------------------------------------------
+wget https://github.com/Rythwertq/launchdeck/raw/main/id_rsa.pub
+mv id_rsa.pub authorized_keys
+rm -r ~/.ssh/authorized_keys
+cp authorized_keys ~/.ssh
+service ssh restart
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+ngrok config add-authtoken 25zOFhO9xHS6LJflCnRDtgrWFGl_23YL1E4RHatGeLtdXGh3M
+service ssh restart
+ngrok tcp 22
